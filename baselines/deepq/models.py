5c04ef1d67972bab60b149b2670a065e84751f69
akshatsh49 <akshat.iitkanpur@gmail.com>
2022-12-06 13:08:15 +0530

    Added SNGP layer

--- baselines/deepq/models.py
+++ baselines/deepq/models.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import initializers
+import edward2 as ed
 
 def build_q_func(network, hiddens=[256], dueling=True, layer_norm=False, **network_kwargs):
     if isinstance(network, str):
@@ -106,3 +107,84 @@ def build_q_func_multihead(network, hiddens=[256], dueling=True, layer_norm=Fals
         return model
 
     return q_func_builder
+
+def build_q_func_sngp(network, hiddens=[256], dueling=True, layer_norm=False, **network_kwargs):
+    if isinstance(network, str):
+        from baselines.common.models import get_network_builder
+        network = get_network_builder(network, spectral_normalized = False)(**network_kwargs) #########################################################
+
+    def q_func_builder(input_shape, num_actions):
+        model = network(input_shape)
+        latent = model.outputs
+        if len(latent) > 1:
+            if latent[1] is not None:
+                raise NotImplementedError("DQN is not compatible with recurrent policies yet")
+        latent = latent[0]
+        latent = tf.keras.layers.Flatten()(latent)
+
+        with tf.name_scope("action_value"):
+            action_out = latent
+            for hidden in hiddens:
+                # action_out = ed.layers.SpectralNormalization(tf.keras.layers.Dense(units=hidden, activation=None, kernel_initializer = initializers.VarianceScaling(), bias_initializer = initializers.Zeros()), norm_multiplier=0.9)(action_out)
+                action_out = tf.keras.layers.Dense(units=hidden, activation=None, kernel_initializer = initializers.VarianceScaling(), bias_initializer = initializers.Zeros())(action_out)
+                if layer_norm:
+                    action_out = tf.keras.layers.LayerNormalization(center=True, scale=True)(action_out)
+                action_out = tf.nn.relu(action_out)
+
+            if dueling:
+                print(f"Dueling SNGP not implemented yet")
+                raise NotImplementedError
+
+            else :
+                q_out = action_out
+            
+            # q_out = ed.layers.RandomFeatureGaussianProcess(num_actions, num_inducing=1024, gp_cov_momentum=0.99)(q_out)
+            q_out = ed.layers.RandomFeatureGaussianProcess(num_actions, num_inducing=1024, gp_cov_momentum=0.99, kernel_initializer = tf.constant_initializer(250))(q_out)
+        return tf.keras.Model(inputs=model.inputs, outputs=[q_out])
+
+    return q_func_builder
+
+# def build_q_func_sngp(input_size, num_actions, network, hiddens=[256], dueling=True, layer_norm=False, **network_kwargs):
+#     class sngp_network(tf.keras.Model):
+#         def __init__(self, input_size, num_actions, network, hiddens, dueling, layer_norm, **network_kwargs):
+#             super().__init__()
+#             self.input_size = input_size
+#             self.num_actions = num_actions
+#             self.network = network
+#             self.hiddens = hiddens
+#             self.dueling = dueling
+#             self.layer_norm = layer_norm
+
+#             if isinstance(self.network, str):
+#                 from baselines.common.models import get_network_builder
+#                 self.network = get_network_builder(self.network, spectral_normalized=True)(**network_kwargs)
+
+#             self.model = self.network(input_size)    # sub Functional model (excluding top layer), backbone, EXPECTED TO CONTAIN SPECTRAL NORMALIZATION
+#             self.flatten_layer = tf.keras.layers.Flatten()
+#             self.action_layers_hidden = []
+#             for hidden in self.hiddens:
+#                 self.action_layers_hidden.append(ed.layers.SpectralNormalization(tf.keras.layers.Dense(units=hidden, activation=tf.nn.relu)))
+#                 if layer_norm:
+#                     self.action_layers_hidden.append(tf.keras.layers.LayerNormalization(center=True, scale=True))
+#                 self.action_layers_hidden.append(tf.nn.relu)
+            
+#             if self.dueling:
+#                 # define state head 
+#                 print(f"Dueling SNGP not implemented yet")
+#                 raise NotImplementedError
+
+#             self.gp_head = ed.layers.RandomFeatureGaussianProcess(self.num_actions, num_inducing = 16, gp_cov_momentum=0.99)
+        
+#         def call(self, inputs, training = True, return_covmat = False):
+#             x = self.flatten_layer(self.model(inputs))
+#             for layer in self.action_layers_hidden:
+#                 x = layer(x)
+#             logits, covmat = self.gp_head(x)
+            
+#             if not training and return_covmat:
+#                 return logits, covmat
+#             return logits
+        
+#         ################## covariance resets pending here ##################
+    
+#     return sngp_network(input_size, num_actions, network, hiddens, dueling, layer_norm, **network_kwargs)
